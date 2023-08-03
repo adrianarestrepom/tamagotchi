8714b6189d9f8de78f531e87431c95151b373056
Adriana Restrepo <adrianarestrepom@hotmail.com>
2023-08-02 20:22:43 -0500

    Segundo commit

--- Scrips.js
+++ Scrips.js
@@ -1,20 +1,34 @@
+const tamago = document.getElementsByClassName('tamagochi');
+const petImage = document.getElementById('eating');
+const petEnergy = document.querySelector('.energy-number');
+const buttonSinging = document.getElementById("buttonSinging");
+let petType = "";
+let energy = 100;
+let hunger = 0;
+let status;
+let petName = "";
 
 
 
 function Init(pet){
     const petName = pet;
-    let energy = 100;
-    let activity ="";
+    
     petEnergy.textContent = `Energy:${energy}`;
 
     return{
-        slepp: function () {
+        singing: function () {
             if (energy <= 90){
                 petImage.src = 'assets/joven.png';
                 petMessage.textContent = `${petName} is sleeping`;
                 activity = "slepping";
                 energy += 10;
+                petImage.src = "https://tenor.com/view/pokemon-jigglypuff-happy-smile-gif-4707068";
                 petEnergy.textContent = `Energy:${energy}`;
+                petMessage.textContent = `${petName} is sleeping`;
+                status = "singing";
+       
+        
+        
 
                 return {
                     energy,
@@ -27,3 +41,7 @@ function Init(pet){
 
     }
 }
+
+const myPet = Init();
+
+buttonSinging.addEventListener('click', () => myPet.singing());
