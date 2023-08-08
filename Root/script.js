477395f3e990f008ca29d1c2149270040f20e82b
Adriana Restrepo <adrianarestrepom@hotmail.com>
2023-08-08 00:32:16 -0500

    Actualizacion js y css

--- Root/script.js
+++ Root/script.js
@@ -1,31 +1,105 @@
+const botonInicio = document.getElementById("botonInicio");
+const botonDormir = document.getElementById("botonDormir");
+const botonComer = document.getElementById("botonComer");
+const botonEstirar = document.getElementById("botonEstirar");
+const botonPensar = document.getElementById("botonPensar");
+const imagen = document.getElementById("imagen");
+const valorEnergia = document.getElementById("energia");
+const estadoElemento = document.getElementById("estado");
+const estado = "";
+const energia = 100;
 
-const botonDormir = document.getElementById('botonDormir');
-const botonComer = document.getElementById('botonComer');
-const botonEstirar = document.getElementById('botonEstirar');
-const botonPensar = document.getElementById('botonPensar');
-const botonInicio = document.getElementById('botonInicio');
-const imagen = document.getElementById('imagen');
+const tamagotchi = {
+  estado: "Feliz",
+  energia: 100,
 
+  comer: function () {
+    this.estado = "Satisfecho";
+    this.energia += 10;
+    actualizarEnergia();
+    actualizarEstado();
+    activarBotones();
+  },
+  dormir: function () {
+    this.estado = "Descansando";
+    this.energia += 20;
+    actualizarEnergia();
+    actualizarEstado();
+    activarBotones();
+  },
+  ejercitarse: function () {
+    this.estado = "Estirando";
+    this.energia -= 15;
+    actualizarEnergia();
+    actualizarEstado();
+  },
+  pensar: function () {
+    this.estado = "Programando";
+    this.energia -= 5;
+    actualizarEnergia();
+    actualizarEstado();
+  },
+  morir: function () {
+    this.estado = "Sin Energia";
+    this.energia = 0;
+    desactivarBotones();
+    actualizarEstado();
+  },
+};
 
-// Agrega manejadores de eventos a los botones
-botonInicio.addEventListener('click', function() {
-    imagen.src = `./assests/Bienvenida.gif`; // Cambia la ruta por la ruta real de tu imagen
+actualizarEstado(); // Actualiza el estado inicial
+actualizarEnergia(); // Actualiza la energía inicial
+
+botonInicio.addEventListener("click", function () {
+  imagen.src = `./assests/Bienvenida.gif`;
 });
 
-botonDormir.addEventListener('click', function() {
-    imagen.src = `./assests/poohDormido.gif`; // Cambia la ruta por la ruta real de tu imagen
+botonDormir.addEventListener("click", function () {
+  imagen.src = `./assests/poohDormido.gif`;
+  tamagotchi.dormir();
 });
 
-botonComer.addEventListener('click', function() {
-    imagen.src = `./assests/poohComiendo.gif`; // Cambia la ruta por la ruta real de tu imagen
+botonComer.addEventListener("click", function () {
+  imagen.src = `./assests/poohComiendo.gif`;
+  tamagotchi.comer();
+});
+botonEstirar.addEventListener("click", function () {
+  imagen.src = `./assests/poohEstirando.gif`;
+  tamagotchi.ejercitarse();
+});
+botonPensar.addEventListener("click", function () {
+  imagen.src = `./assests/poohPensando.gif`;
+  tamagotchi.pensar();
 });
 
-botonEstirar.addEventListener('click', function() {
-    imagen.src = `./assests/poohEstirando.gif`; // Cambia la ruta por la ruta real de tu imagen
-});
+function actualizarEstado() {
+  estadoElemento.textContent = tamagotchi.estado;
+}
 
-botonPensar.addEventListener('click', function() {
-    imagen.src = `./assests/poohPensando.gif`; // Cambia la ruta por la ruta real de tu imagen
-});
+function actualizarEnergia() {
+  valorEnergia.textContent = tamagotchi.energia;
+  if (tamagotchi.energia <= 0) {
+    tamagotchi.morir();
+  }
+}
 
+function desactivarBotones() {
+  botonEstirar.disabled = true;
+  botonPensar.disabled = true;
+  botonComer.disabled = false;
+  botonDormir.disabled = false;
+}
 
+function activarBotones() {
+  botonEstirar.disabled = false;
+  botonPensar.disabled = false;
+}
+
+setInterval(function () {
+  imagen.src = `./assests/Bienvenida.gif`;
+
+  if (tamagotchi.energia > 0) {
+    tamagotchi.energia -= 10;
+    actualizarEnergia();
+  }
+}, 5000);
